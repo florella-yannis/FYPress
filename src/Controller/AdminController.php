3e3f2cb8d29553463690556a183d05a9155e994e
florella-yannis <florellayannis@gmail.com>
2023-04-13 23:22:25 +0200

    show user + modify user role

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -25,9 +27,19 @@ class AdminController extends AbstractController
         }
 
         $categories = $entityManager->getRepository(Category::class)->findBy(['deletedAt' => null]);
+<<<<<<< Updated upstream
 
         return $this->render('admin/show_dashboard.html.twig', [
             'categories' => $categories
+=======
+        $articles = $entityManager->getRepository(Article::class)->findBy(['deletedAt' => null]);
+        $users = $entityManager->getRepository(User::class)->findBy(['deletedAt' => null]);
+
+        return $this->render('admin/show_dashboard.html.twig', [
+            'categories' => $categories,
+            'articles' => $articles,
+            'users' => $users
+>>>>>>> Stashed changes
         ]);
     } // end showDashboard()
 
@@ -35,10 +47,38 @@ class AdminController extends AbstractController
     public function showArchives(EntityManagerInterface $entityManager): Response
     {
         $categories = $entityManager->getRepository(Category::class)->findAllArchived();
+<<<<<<< Updated upstream
 
 
         return $this->render('admin/show_archive.html.twig', [
             'categories' => $categories
+=======
+        $articles = $entityManager->getRepository(Article::class)->findAllArchived();
+
+
+        return $this->render('admin/show_archive.html.twig', [
+            'categories' => $categories,
+            'articles' => $articles
+>>>>>>> Stashed changes
         ]);
-    }
+    } // end showArchives()
+
+    #[Route('/modifier-role-user/{id}', name: 'modify_user_role', methods: ['GET'])]
+    public function modifyUserRole(User $user, UserRepository $repository): Response
+    {
+        # - 1 : recuperer son role 2 : modifier le role 3 : save en BDD
+
+        if(in_array('ROLE_USER', $user->getRoles())) {
+            $user->setRoles(['ROLE_ADMIN']);
+        }
+        else {
+            $user->setRoles(['ROLE_USER']);
+        }
+
+        $repository->save($user, true);
+
+        $this->addFlash('success', "Le rôle a bien été modifié.");
+        return $this->redirectToRoute('show_dashboard');
+    } // end modifyRole()
+
 } // end class
