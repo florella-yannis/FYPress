91e77c4228cd5c71d6eb6c33e47896470d0b0565
florella-yannis <florellayannis@gmail.com>
2023-04-14 00:34:18 +0200

    add ago twig + show one article

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,15 +2,45 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Entity\Category;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-    #[Route('/', name: 'show_home', methods:['GET'])]
-    public function showHome(): Response
+    #[Route('/', name: 'show_home', methods: ['GET'])]
+    public function showHome(ArticleRepository $repository): Response
     {
-        return $this->render('default/show_home.html.twig');
+        $articles = $repository->findBy(['deletedAt' => null]);
+
+        return $this->render('default/show_home.html.twig', [
+            'articles' => $articles
+        ]);
+    } // end function showHome()
+
+    #[Route('/voir-articles/{alias}', name: 'show_articles_from_cat', methods: ['GET'])]
+    public function showArticlesFromCategory(Category $category, ArticleRepository $articleRepository): Response
+    {
+        $articles = $articleRepository->findBy([
+            'deletedAt' => null,
+            'category' => $category->getId()
+        ]);
+
+        return $this->render('default/show_articles_from_cat.html.twig', [
+           'articles' => $articles,
+           'category' => $category
+        ]);
+    } // end showFromCat()
+
+    #[Route('/{cat_alias}/{art_alias}_{id}.html', name: 'show_article', methods: ['GET'])]
+    public function showArticle(Article $article): Response
+    {
+        return $this->render('default/show_article.html.twig', [
+            'article' => $article
+        ]);
     }
-}
+
+} // end class
